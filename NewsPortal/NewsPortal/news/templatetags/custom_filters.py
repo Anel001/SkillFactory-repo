506aa456f64aa1a467a652cda67ee8e9b257fb0b
Anel <anel04tileulieva05@gmail.com>
2022-02-11 00:04:47 +0300

    Init Skillfactory

---
+++ NewsPortal/NewsPortal/news/templatetags/custom_filters.py
@@ -0,0 +1,19 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='censor')
+def censor(value):
+    list = ['екалэмэнэ',
+            'дурак',
+            'фиг']
+
+    if isinstance(value, str):
+        for i in list:
+            for j in value:
+                if j == i:
+                    j = "***"
+        return str(value)
+    else:
+        raise ValueError(f' {type(value)} не является строкой')
