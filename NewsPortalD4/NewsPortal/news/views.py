370e1f537b38b4cd1ea2b84619d105e0925c7e0c
Anel <anel04tileulieva05@gmail.com>
2022-02-23 17:05:22 +0300

    Init Skillfactory

---
+++ NewsPortalD4/NewsPortal/news/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, CreateView, DetailView, DeleteView
+from django.core.paginator import Paginator
+from .models import Post, Category
+from .forms import PostForm
+from .filters import PostFilter
+from datetime import datetime
+
+
+class PostList(ListView):
+    model = Post
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.order_by('-id')
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class SearchList(ListView):
+    model = Post
+    template_name = 'searchposts.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['filter'] = PostFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class PostCreateView(CreateView):
+    template_name = 'create.html'
+    form_class = PostForm
+
+
+class PostUpdateView(UpdateView):
+    template_name = 'create.html'
+    form_class = PostForm
+
+    def get_object(self, **kwargs):
+        id = self.kwargs.get('pk')
+        return Post.objects.get(pk=id)
+
+
+class PostDeleteView(DeleteView):
+    template_name = 'delete.html'
+    queryset = Post.objects.all()
+    success_url = '/posts/'
+
