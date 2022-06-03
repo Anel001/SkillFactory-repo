5b8b4c9e4aff3c98d96be7da3605baf8df632287
Anel001 <92825585+Anel001@users.noreply.github.com>
2022-06-03 09:09:40 +0300

    Delete NewsPortalD5 directory

--- NewsPortalD5/NewsPortal/news/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Post
-
-
-# Создаём модельную форму
-class PostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['caption', 'text', 'category', 'Author']
